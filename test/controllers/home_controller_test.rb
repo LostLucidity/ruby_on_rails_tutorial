a5bb9da81012ed5e1702ed4a085841eb0647a7fb
Daniel Munoz <lostlucidity@gmail.com>
2015-12-08 13:04:40 -0500

    fixed a link, added integration test for site layout, disabled users test since it belongs to another project.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,8 +5,8 @@ class HomeControllerTest < ActionController::TestCase
   #   assert true
   # end
 
-  test "should get index" do
-	  get :index
-	  assert_response :success
-	end
+  # test "should get index" do
+	 #  get :index
+	 #  assert_response :success
+	# end
 end
